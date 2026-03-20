cd9cea9313dfe4a8796c58dc132326aac6f02734
Tanvi <tanvi.career@gmail.com>
2026-03-21 01:54:57 +0530

    Added basic frontend pages and routing

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "AI Coding Mentor API is running 🚀"}
+@app.get("/login-page", response_class=HTMLResponse)
+def login_page():
+    with open("templates/login.html") as f:
+        return f.read()
 
-class Question(BaseModel):
-    message: str
+@app.get("/signup-page", response_class=HTMLResponse)
+def signup_page():
+    with open("templates/signup.html") as f:
+        return f.read()
 
-@app.post("/chat")
-def chat(q: Question):
-    return {"reply": "This is a placeholder response"}
+@app.get("/chat-page", response_class=HTMLResponse)
+def chat_page():
+    with open("templates/chat.html") as f:
+        return f.read()
